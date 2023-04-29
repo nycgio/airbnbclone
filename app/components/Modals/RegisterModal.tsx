0b69e2fc10142bb6ae0ae59b472405d0b8f8c792
nycgio <giorgialvarez@gmail.com>
2023-04-29 00:39:45 -0400

    Added buttons to the register modal

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -5,11 +5,13 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
+import { toast } from 'react-hot-toast';
 
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Button from '../Button';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -35,7 +37,7 @@ const RegisterModal = () => {
         axios.post('/api/register', data).then(() => {
             registerModal.onClose();
         }).catch((err) => {
-            console.log(err);
+            toast.error('Something went wrong');
         }).finally(() => {
             setIsLoading(false)
         })
@@ -55,6 +57,63 @@ const RegisterModal = () => {
                 errors={errors}
                 required
             />
+
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+
+            <Input
+                id="password"
+                type='password'
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+                outline
+                label='Continue with Google'
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+
+            <Button
+                outline
+                label='Continue with Github'
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div
+                className="
+                    text-neutral-500
+                    text-center
+                    mt-4
+                    font-light
+                "
+            >
+                <div className="flex flex-row items-center justify-center gap-2">
+                   <div>
+                        Already have an account ?
+                   </div>
+                   <div 
+                        onClick={registerModal.onClose}
+                        className="text-neutral-800 cursor-pointer hover:underline">
+                        Login
+                   </div>
+                </div>
+            </div>
         </div>
     )
 
@@ -67,6 +126,7 @@ const RegisterModal = () => {
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
         body={bodyContent}
+        footer={footerContent}
     />
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from 'next/font/google'
 import ClientOnly from './components/ClientOnly'
 import Navbar from './components/navbar/Navbar'
 import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = Nunito({
   subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
